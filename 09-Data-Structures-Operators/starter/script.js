4933dd97b6e2ec73c203b97ea377ce9e66ef419e
hiren100 <hirenramoliya95@gmail.com>
2022-06-14 17:39:29 +0530

    functions code

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 
@@ -137,7 +135,7 @@ const game = {
   },
 };
 
-const [players1,players2] = game.players;
+/* const [players1,players2] = game.players;
 
 const [gk, ...fieldPlayers] = players1;
 
@@ -153,15 +151,15 @@ const {odds: {team1, x: draw, team2}} = game;
 
 const printGoals = function (...players){
     console.log(`${players.length} golas were socred`)
-}
+} */
 
 /* printGoals("Davies","Muller", "Lewandowski","Kimmich")
 printGoals("Davies","Muller",) */
-printGoals(...game.players)
+//printGoals(...game.players)
 
 // 7.
 
-team1 > team2 && console.log('Team 2 is likely to win')
+/* team1 > team2 && console.log('Team 2 is likely to win')
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
 
@@ -182,4 +180,220 @@ for (const day of days) {
   console.log(`On ${day}, we open at ${open}`);
 }
 
-console.log(restaurant.order?.(0,1) ?? "Method doesn't exist")
+console.log(restaurant.order?.(0,1) ?? "Method doesn't exist") */
+
+// challenge 2 
+
+//1.
+/* for (const [i,player] of game.scored.entries()){
+  console.log(`Goal ${i+1}: ${player}`)
+}
+
+//2
+
+const oddsValue = Object.values(game.odds);
+console.log(oddsValue)
+let average = 0;
+for (const odd of oddsValue) average += odd; average = average / oddsValue.length
+ console.log(average)
+
+
+ // 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+} */
+
+
+//sets
+
+/* const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+console.log(ordersSet)
+  
+  for(const order of ordersSet)  console.log(order)
+
+  //example of set
+
+  const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+  //const staffUnique = [...new Set(staff)];
+  const staffUnique = Array.from(new Set(staff));
+  console.log(staffUnique);
+
+
+  // Map
+  const rest = new Map();
+  rest.set('name', 'Classico Italiano');
+  rest.set(1, 'Firenze, Italy');
+  console.log(rest.set(2, 'Lisbon, Portugal'));
+  
+  rest
+    .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open :D')
+    .set(false, 'We are closed :(');
+  
+  console.log(rest.get('name'));
+  console.log(rest.get(true));
+  console.log(rest.get(1));
+
+  rest.set(document.querySelector('h1', 'Heading'))
+
+
+
+
+  const question = new Map([
+    ['question', 'What is the best programming language in the world?'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    ['correct', 3],
+    [true, 'Correct 🎉'],
+    [false, 'Try again!'],
+  ]);
+
+  question.set(34, 'Score')
+
+  console.log(question); */
+
+  // for (const [key,value] of question) 
+  // if(typeof key === "number"){
+  //   console.log(`Answer is ${key}: ${value}`)
+  // }
+
+  /* const answer =Number(prompt('Your answer Number'))
+  
+  console.log(question.get(question.get('correct') === answer)); */
+
+  // Convert map to array
+/* console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]); */
+
+
+
+/* const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+  ]);
+
+  //1.
+
+  
+  const gameEventsArr = [...gameEvents.values()]
+
+  const events = Array.from(new Set(gameEventsArr))
+
+  //const events = [...new Set(gameEvents.values())];
+
+  console.log(events)
+
+  gameEvents.delete(64);
+
+  console.log(
+    `An event happened, on average, every ${90 / gameEvents.size} minutes`
+  );
+
+  const time2 = [...gameEvents.keys()].pop();
+console.log(time2);
+console.log(
+  `An event happened, on average, every ${time2 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+} */
+
+// Strings tuts
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+/* console.log(airline.indexOf('l'))
+
+console.log(airline.slice(4));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); */
+
+/* const checkMiddleSeat = function (seat) {
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+  else console.log('You got lucky 😎');
+}
+
+checkMiddleSeat('11B')
+checkMiddleSeat('23C')
+checkMiddleSeat('3E')
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+ console.log(announcement.replaceAll('door', 'gate'));
+
+ console.log('a+very+nice+string'.split('+'));
+ console.log('Jonas Schmedtmann'.split(' '));
+
+ const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)} and ${'😎'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12); */
+
+document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+const b = document.createElement('button');
+
+b.textContent = 'Click';
+document.body.append(b);
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+      )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+  const h = flights.split('+');
+ 
+
+  for (const flight of flights.split('+')){
+    const [type, from, to, time] = flight.split(';');
+    const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} from ${from.slice(0,3).toUpperCase()} ${to.replaceAll(to, 'to')} ${time}`
+    console.log(output.padStart(28))
+  }
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,271 @@
 'use strict';
+
+const flight = "LH234"
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function(flightNum,passenger){
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  // if (passenger.passport === 24739479284) {
+  //   alert('Checked in');
+  // } else {
+  //   alert('Wrong passport!');
+  // }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Functions Accepting Callback Functions
+/* const oneWord = function (str) {
+  return str.replaceAll(/ /g,'').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+
+const high5 = function () {
+  console.log('👋');
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greeterHey = greet('Hey')
+greeterHey('Hiren')
+greeterHey('Amit')
+
+greet('hi')('jonas')
+
+
+const greetArrow = (greeting) => {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+}
+
+greetArrow('hi')('jonas')
+
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+
+lufthansa.planes = 300;
+lufthansa.buyPlanes = function(){
+  this.planes++;
+  console.log(this.planes)
+}
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlanes.bind(lufthansa))
+
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// Functions Returning Functions
+const addTaxCalc = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+}
+
+const addTaxVat = addTaxCalc(0.23);
+
+console.log(addTaxVat(34)) */
+
+
+//challenge 1
+
+
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer : function(){
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+    // Register answer
+
+    if(typeof answer === 'number' && answer < this.answers.length){
+      this.answers[answer]++;
+    }
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+
+
+//poll.registerNewAnswer()
+
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+
+//invoked function
+
+const runOnce = function(){
+  console.log('This is never run agian!')
+}
+runOnce();
+
+// IIFE
+// (function () {
+//   console.log('This will never run again');
+//   const isPrivate = 23;
+// })();
+
+
+// (() => console.log('This will ALSO never run again'))();
+
+///////////////////////////////////////
+// Closures
+
+// A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). 
+// In other words, a closure gives you access to an outer function's scope from an inner function.
+
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g()
+f()
+h()
+f()
+
+const boardPassengers = function(n,wait){
+  const perGroup = n/3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000)
+
+  console.log(`Will start boarding in ${wait} seconds`);
+}
+
+const perGroup = 1000;
+boardPassengers(21,2)
+
+
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+//   document.querySelector('body').addEventListener('click', function () {
+//     header.style.color = 'blue';
+//   });
+// })();
