c05876d8c48997c9562d614042a0dee61672c522
hiren100 <hirenramoliya95@gmail.com>
2022-07-08 15:37:34 +0530

    javascript code

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,16 +7,26 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+  console.log('modal class contain '+!modal.classList.contains('hidden'))
 };
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+  console.log('modal class contain '+!modal.classList.contains('hidden'))
 };
+
+
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -28,4 +38,287 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const h1 = document.querySelector('h1')
+
+h1.addEventListener('mouseenter',function(e){
+  h1.style.color = "indigo";
+})
+
+const randomInt = (min, max) =>{
+ const test = (max - min + 1) + min
+ console.log(Math.random() * test)
+ 
+  Math.floor(Math.random() * (max - min + 1) + min);
+}
+randomInt(0,255)
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+//   console.log(randomColor(0,255))
+
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+// dom traversing
+[...h1.parentElement.children].forEach(function(el){
+  if (el !== h1)
+  el.style.transform = "scale(0.5)"
+})
+
+// Tabbed component
+
+/* tabs.forEach(function(tb){
+  tb.addEventListener('click', function(){
+
+
+  })
+}) */
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+  .classList.add('operations__content--active');
+})
+
+// Menu fade animation
+
+const handleHover = function(e){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const sibling = link.closest('.nav').querySelectorAll('.nav__link')
+    const logo = link.closest('.nav').querySelector('img')
+
+    sibling.forEach(el => {
+      if(el !== link )  el.style.opacity = this;      
+    })
+    logo.style.opacity = this
+  }
+};
+
+/* nav.addEventListener('mouseover',function(e){
+  handleHover(e,0.5)
+})
+nav.addEventListener('mouseout',function(e){
+  handleHover(e,1)
+}) */
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+
+/* 
+Old way code
+nav.addEventListener('mouseover', function(e){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const sibling = link.closest('.nav').querySelectorAll('.nav__link')
+    const logo = link.closest('.nav').querySelector('img')
+
+    sibling.forEach(el => {
+      if(el !== link )  el.style.opacity = 0.5;
+
+      logo.style.opacity = 0.5
+
+    })
+  }
+})
+
+nav.addEventListener('mouseout', function(e){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const sibling = link.closest('.nav').querySelectorAll('.nav__link')
+    const logo = link.closest('.nav').querySelector('img')
+    
+    sibling.forEach(el => {
+      if(el !== link )  el.style.opacity =1;
+    })
+    logo.style.opacity =1;
+  }
+}) */
+
+// Sticky nav old way
+/* const section1Cords = section1.getBoundingClientRect()
+window.addEventListener('scroll',function(){
+  console.log(section1Cords.top)
+  if(this.window.scrollY > section1Cords.top){
+    nav.classList.add('sticky') 
+  }else if(this.window.scrollY < section1Cords.top){
+    nav.classList.remove('sticky')
+  }
+}) */
+
+// Sticky navigation: Intersection Observer API
+const header = document.querySelector('.header');
+
+const handleStickyNav = function(entries){
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      nav.classList.remove('sticky')
+    } else {
+      nav.classList.add('sticky')
+    }
+  });
+}
+
+const observer = new IntersectionObserver(handleStickyNav);
+
+observer.observe(header);
+
+
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
 });
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,225 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+
+/* const countriesData = function(country){
+
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+request.send()
+
+
+
+request.addEventListener('load',function(){
+  //console.log(this.responseText)
+
+  const data = JSON.parse(this.responseText)
+  console.log(data[0])
+  const html = `<article class="country">
+  <img class="country__img" src="${data[0].flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data[0].name.common}</h3>
+    <h4 class="country__region">${data[0].region}</h4>
+    <p class="country__row"><span>👫</span>${(data[0].population / 1000000).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️ </span>${data[0].languages.por}</p>
+    <p class="country__row"><span>💰</span>${data[0].currencies.EUR.name}</p>
+  </div>
+</article>`
+
+countriesContainer.insertAdjacentHTML('beforeend', html);
+countriesContainer.style.opacity = 1;
+
+//console.log(data[1])
+
+
+// const mapdata = data.map(m => m.maps)
+
+// console.log(mapdata[1].googleMaps)
+
+})
+
+}
+
+countriesData('Portugal') */
+
+
+// Consuming Promises
+// Chaining Promises
+// Handling Rejected Promises
+// Throwing Errors Manually
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+
+const renderCountry = function (data, className = '') {
+
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+
+
+
+// const getCountryData = function(country){
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//   .then(function(response){
+//     if(!response.ok){
+//       throw new Error(`Country not found (${response.status})`) 
+//     }
+//     return response.json()
+//   })
+//   .then(function(data){
+//     renderCountry(data[0])
+//     const neighbour = data[0].borders[0]
+//     if(!neighbour) return;
+
+//     return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+//   })
+//   .then(function(response){
+//     if(!response.ok){
+//       throw new Error(`Country not found (${response.status})`) 
+//     }
+//     return response.json()
+//   })
+//   .then(function(data){
+//     renderCountry(data, 'neighbour')
+//   })
+//   .catch(err => {
+//     console.error(`${err} 💥💥💥`);
+//     renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//   })
+//   .finally(() => {
+//     countriesContainer.style.opacity = 1;
+//   });
+// }
+
+// getCountryData('USA')
+
+
+
+//challenge 
+
+/* const whereAmI = function(lat, lng){
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+  .then(function(response){
+    if(!response.ok){
+      throw new Error(`Problem with geocoding (${response.status})`) 
+    }
+    return response.json()
+  })
+  .then(function(data){
+    //console.log(data)
+    console.log(`You are in ${data.city}, ${data.country}`);
+    return fetch(`https://restcountries.com/v2/name/${data.country}`);
+  })
+  .then(res => {
+    if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+    return res.json();
+  })
+  .then(data => {
+    console.log(data[0])
+    renderCountry(data[0])
+  })
+  .catch(err => {
+    console.error(`${err.message} 💥💥💥`);
+  })
+}
+
+whereAmI(52.508, 13.381); */
+
+
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+// getPosition().then(pos => console.log(pos));
+
+const whereAmI = function () {
+    getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+
+     // console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[1]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+
+
+whereAmI()
+
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+    console.log(data[0])
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
